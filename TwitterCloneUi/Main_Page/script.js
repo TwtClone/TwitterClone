5375a4be57f93173d8df5712487c9a45766f83d8
Jisaac5 <jisaacserote@gmail.com>
2024-02-14 23:19:21 +0800

    Moved register/login functions to login page, copies left in main page for testing

--- TwitterCloneUi/Main_Page/script.js
+++ TwitterCloneUi/Main_Page/script.js
@@ -122,70 +122,70 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-//Register (NOTE: Move to login page)
-async function apiReg(){
-  try{
-    const req = await fetch("http://localhost:3000/api/v1/auth/register", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "username": "terry",
-        "password": "wahoo"
-      })
-    });
-    if (req.ok){
-      const reqData = await req.json;
-      console.log("User created: ", reqData);
-    }
-    else{
-      const reqError = await req.json;
-      throw(reqError);
-    }
-  }
-  catch(reqError){
-    console.error('Error occurred during registration: ', reqError);
-  }
-}
+//Register (Copied from login scripts, for testing)
+// async function apiReg(username, password){
+//   try{
+//     const req = await fetch("http://localhost:3000/api/v1/auth/register", {
+//       method: "POST",
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({
+//         "username": username,
+//         "password": password
+//       })
+//     });
+//     if (req.ok){
+//       const reqData = await req.json;
+//       console.log("User created: ", reqData);
+//     }
+//     else{
+//       const reqError = await req.json;
+//       throw(reqError);
+//     }
+//   }
+//   catch(reqError){
+//     console.error('Error occurred during registration: ', reqError);
+//   }
+// }
 
-//Log In (NOTE: Move to login page)
-async function apiLogin(username, password){
-  try{
-    const req = await fetch("http://localhost:3000/api/v1/auth/login", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "username": username,
-        "password": password
-      })
-    });
-    // .then()
-    // // .then(res => res.json())
-    // .then(data => console.log("Data: ", data))
-    // .then(console.log("aaaaa"))
-    //   .catch(error => console.log("An error has occurred: ", error))
+//Log In (Copied from login scripts, for testing)
+// async function apiLogin(username, password){
+//   try{
+//     const req = await fetch("http://localhost:3000/api/v1/auth/login", {
+//       method: "POST",
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({
+//         "username": username,
+//         "password": password
+//       })
+//     });
+//     // .then()
+//     // // .then(res => res.json())
+//     // .then(data => console.log("Data: ", data))
+//     // .then(console.log("aaaaa"))
+//     //   .catch(error => console.log("An error has occurred: ", error))
     
-    if(req.ok){
-      const authToken = await req.text();
-      localStorage.setItem('authToken', authToken);
-      console.log("Login successful. Username:", username)
-      // var appendtest = 'Bearer '+authToken
-      // console.log('Append test', appendtest); 
-      console.log('Token: ',authToken); //For debug, logs fetched token. Maybe comment out later
-    }
-    else{
-      const reqError = await req.text();
-      console.log('Login error: ',reqError);
-      console.error(reqError);
-    }
-  }
-  catch(reqError){
-    console.error('Error occurred during login: ', reqError);
-  }
-}
+//     if(req.ok){
+//       const authToken = await req.text();
+//       localStorage.setItem('authToken', authToken);
+//       console.log("Login successful. Username:", username)
+//       // var appendtest = 'Bearer '+authToken
+//       // console.log('Append test', appendtest); 
+//       console.log('Token: ',authToken); //For debug, logs fetched token. Maybe comment out later
+//     }
+//     else{
+//       const reqError = await req.text();
+//       console.log('Login error: ',reqError);
+//       console.error(reqError);
+//     }
+//   }
+//   catch(reqError){
+//     console.error('Error occurred during login: ', reqError);
+//   }
+// }
 
 //For debug: Function testing
 // apiReg();
